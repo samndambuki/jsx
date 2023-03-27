562a92d0910de895dde89551296976e7c89d2560
samndambuki <sam.ndambuki08@gmail.com>
2023-03-27 14:56:28 +0300

    markup using jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function TodoList(){
+  return(
+    <>
+    <h1>Samuel Ndambuki Todos</h1>
+    <ul>
+      <li>Invent new traffic lights</li>
+      <li>Rehearse new movie scene </li>
+      <li>Improve spectrum technology</li>
+    </ul>
+    </>
+  )
+}
